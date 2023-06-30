80cbf44ac36d6b1b0b2ec21ccd6e972dd433afa9
Cristhian Garcia <crisgarta8@gmail.com>
2023-06-30 11:56:17 -0500

    feat: allow to import dashboards roles

--- tutoraspects/templates/aspects/jobs/init/superset/init-superset.sh
+++ tutoraspects/templates/aspects/jobs/init/superset/init-superset.sh
@@ -39,24 +39,24 @@ EOF
 }
 # Initialize the database
 echo_step "1" "Starting" "Applying DB migrations"
-superset db upgrade
-superset init
+#superset db upgrade
+#superset init
 echo_step "1" "Complete" "Applying DB migrations"
 
 # Create an admin user
 echo_step "2" "Starting" "Setting up admin user"
-superset fab create-admin \
-  --username "{{ SUPERSET_ADMIN_USERNAME }}" \
-  --password "{{ SUPERSET_ADMIN_PASSWORD }}" \
-  --firstname Superset \
-  --lastname Admin \
-  --email "{{ SUPERSET_ADMIN_EMAIL }}"
+#superset fab create-admin \
+#  --username "{{ SUPERSET_ADMIN_USERNAME }}" \
+#  --password "{{ SUPERSET_ADMIN_PASSWORD }}" \
+#  --firstname Superset \
+#  --lastname Admin \
+#  --email "{{ SUPERSET_ADMIN_EMAIL }}"
 
 # Update the password of the Admin user
 # (in case it changed since the user was created)
-superset fab reset-password \
-  --username "{{ SUPERSET_ADMIN_USERNAME }}" \
-  --password "{{ SUPERSET_ADMIN_PASSWORD }}"
+#superset fab reset-password \
+#  --username "{{ SUPERSET_ADMIN_USERNAME }}" \
+#  --password "{{ SUPERSET_ADMIN_PASSWORD }}"
 echo_step "2" "Complete" "Setting up admin user"
 
 # Create default roles and permissions
@@ -66,28 +66,27 @@ echo_step "3" "Complete" "Setting up roles and perms"
 
 echo_step "4" "Starting" "Importing assets"
 
+
 cd /app/assets/
 rm -rf /app/assets/superset
-python /app/pythonpath/create_assets.py
+
+mkdir /app/assets/superset -p
 
 date=$(date -u +"%Y-%m-%dT%H:%M:%S.%6N+00:00") 
-
 echo "version: 1.0.0
-type: Dashboard
+type: assets
 timestamp: '$date'" > superset/metadata.yaml
 
-zip -r superset.zip superset
+python /app/pythonpath/create_assets.py
 
-echo "\n\nImporting zip file\n\n"
-superset import-dashboards -p superset.zip
 
-rm -rf /app/assets/superset
-rm -rf /app/assets/superset.zip
-echo_step "4" "Complete" "Importing assets"
+#rm -rf /app/assets/superset
+#rm -rf /app/assets/superset.zip
+#echo_step "4" "Complete" "Importing assets"
 
 # Set up a Row-Level Security filter to enforce course-based access restrictions.
 # Note: there are no cli commands or REST API endpoints to help us with this,
 # so we have to pipe python code directly into the superset shell. Yuck!
-echo_step "5" "Starting" "Setup row level security filters"
-python /app/pythonpath/create_row_level_security.py
-echo_step "5" "Complete" "Setup row level security filters"
+#echo_step "5" "Starting" "Setup row level security filters"
+#python /app/pythonpath/create_row_level_security.py
+#echo_step "5" "Complete" "Setup row level security filters"
